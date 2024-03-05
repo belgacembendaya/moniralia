97a185fec91fe8403239ef2958549a51b435aed8
belgacembendaya <belgacem.bendaya@gmail.com>
2024-03-05 08:50:31 +0100

    footer creation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  {path:"", component : HomeComponent} ,
   {path:"about", component : AboutComponent} ,
   {path:"home", component : HomeComponent} 
 ];
