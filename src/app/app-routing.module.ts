5bec4befbb871f845571dd4089fcaec88b415813
belgacembendaya <belgacem.bendaya@gmail.com>
2024-03-07 22:40:18 +0100

    2eme modification

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { ContactComponent} from './contact/contact.component';
+import { ProductsComponent } from './products/products.component';
+
 
 const routes: Routes = [
-  {path:"", component : HomeComponent} ,
+  {path:"home", component : HomeComponent} ,
   {path:"about", component : AboutComponent} ,
-  {path:"home", component : HomeComponent} 
+  {path:"contact", component : ContactComponent} ,
+  {path:"products", component : ProductsComponent} 
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AboutComponent,
+    ContactComponent,
+    ProductsComponent,
   
   ],
   imports: [
